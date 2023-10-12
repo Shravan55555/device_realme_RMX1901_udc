0546a2bbc7131e93ea9d77dcae6f7d0fef1db571
Cykeek <Cykeek@proton.me>
2023-10-12 12:52:00 +0530

    RMX1901: Add Keymaster 4.0 Fix: init: Control message: Could not find 'android.hardware.keymaster@4.0::IKeymasterDevice/default' for ctl.interface_start from pid: 511 (/system/bin/hwservicemanager)
    Signed-off-by: Cykeek <Cykeek@proton.me>
    Signed-off-by: ij-israfil <israfilkhan494gmail.com>

--- device.mk
+++ device.mk
@@ -211,7 +211,10 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1.vendor
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.0-impl:64 \
+    android.hardware.keymaster@4.0-service
+
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
