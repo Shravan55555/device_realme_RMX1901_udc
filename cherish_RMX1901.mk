b70c3a1bc93aaafe862a75282a29d4f7b99de51b
Shravankumar <shravankumarsayz@gmail.com>
2024-04-06 09:29:46 +0530

    lineage

--- cherish_RMX1901.mk
+++ cherish_RMX1901.mk
@@ -8,11 +8,19 @@
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+#CHERISH_VANILLA := true
+WITH_GMS := true
+TARGET_USES_MINI_GAPPS := true
+USE_PIXEL_CHARGING := true
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_DISABLE_EPPE := true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=SHRAVAN
 
-
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +30,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := cherish_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_SYSTEM_NAME := RMX1901
