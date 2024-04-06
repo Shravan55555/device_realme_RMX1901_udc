56484d1c5608417f4c17638c7709f535765c8398
Shravankumar <shravankumarsayz@gmail.com>
2024-04-06 09:22:56 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/cherish_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-userdebug \
-    lineage_RMX1901-eng
+    cherish_RMX1901-userdebug \
+    cherish_RMX1901-eng
