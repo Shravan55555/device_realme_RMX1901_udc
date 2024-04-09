01e922c9a6803f54a075d2c95506c14574b39653
Shravankumar <shravankumarsayz@gmail.com>
2024-04-09 13:26:14 +0530

    Update lineage_RMX1901.mk

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -10,7 +10,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
