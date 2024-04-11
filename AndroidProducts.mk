3dae36e7b583e529099c7ed64f9e48be7a27ae20
Shravankumar <shravankumarsayz@gmail.com>
2024-04-11 18:12:28 +0530

    RMX1901: EVEREST

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_RMX1901.mk
+    $(LOCAL_DIR)/everest_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_RMX1901-userdebug \
-    blaze_RMX1901-eng
+    everest_RMX1901-userdebug \
+    everest_RMX1901-eng
