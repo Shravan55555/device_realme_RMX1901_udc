4b63aa56ba357eb2443f5efd892182bb97720620
Shravankumar <shravankumarsayz@gmail.com>
2024-04-11 09:05:17 +0530

    RMX1901: AOSPA

--- aospa_RMX1901.mk
+++ aospa_RMX1901.mk
@@ -12,7 +12,8 @@ TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +23,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := aospa_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_SYSTEM_NAME := RMX1901
