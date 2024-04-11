eb641f6f6090a08065842e06009d0d8412b9d750
shravansayz <shravankumarsayz@gmail.com>
2024-04-11 18:34:45 +0530

    Revert "TEST"
    This reverts commit 49b94d89e14724d2809eef31b6f607cb51f8c7b2, reversing
    changes made to b47b256b53413e729ec7981a32e9aea35c13bed2.

--- aospa_RMX1901.mk
+++ aospa_RMX1901.mk
@@ -7,26 +7,15 @@
 # Inherit some common Pixel Experience stuff
 
 TARGET_BOOT_ANIMATION_RES := 1080
-<<<<<<<< HEAD:aospa_RMX1901.mk
 TARGET_HAS_UDFPS := true
 TARGET_ENABLE_BLUR := true
-========
-EVEREST_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_BLUR := true
-EVEREST_MAINTAINER := SHRAVAN
->>>>>>>> 5015f6b6f30d2184a6c7b74c8600cbb1a1089d30:everest_RMX1901.mk
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
-<<<<<<<< HEAD:aospa_RMX1901.mk
 AOSPA_BUILD_VARIANT := beta
 TARGET_BOARD_PLATFORM := sdm710
 $(call inherit-product, vendor/aospa/target/product/version.mk)
 
-========
-
-$(call inherit-product, vendor/everest/config/common_full_phone.mk)
->>>>>>>> 5015f6b6f30d2184a6c7b74c8600cbb1a1089d30:everest_RMX1901.mk
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -36,11 +25,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-<<<<<<<< HEAD:aospa_RMX1901.mk
 PRODUCT_NAME := aospa_RMX1901
-========
-PRODUCT_NAME := everest_RMX1901
->>>>>>>> 5015f6b6f30d2184a6c7b74c8600cbb1a1089d30:everest_RMX1901.mk
 PRODUCT_MODEL := Realme X
 
 PRODUCT_SYSTEM_NAME := RMX1901
