b25f9bf9065d95d70991f26c09b62f49d3c20ff6
shravansayz <shravankumarsayz@gmail.com>
2024-04-28 13:28:18 +0530

    RMX1901: packages: remove some package

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,31 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-		RecorderPrebuilt \
-		Photos \
-		Drive \
-		Maps \
-		YouTube \
-		MeetPrebuilt \
-		PrebuiltGmail \
-		SoundAmplifierPrebuilt \
-		GoogleCamera \
-		AdaptiveVPNPrebuilt \
-		CarrierLocation \
-		CarrierMetrics \
-		DevicePolicyPrebuilt \
-		DiagnosticsToolPrebuilt \
-		SafetyHubPrebuilt \
-		ScribePrebuilt \
-		Showcase \
-		Tycho \
-		Videos \
-		talkback \
-		Updates \
-		FM2 \
-		NfcNci \
-		AndroidAutoStubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := GoogleCamera AdaptiveVPNPrebuilt CalendarGooglePrebuilt FilesPrebuilt PlayAutoInstallConfig SafetyHubPrebuilt talkback Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
