5008151be75d5400c0006070a49fe308238a1f34
shravansayz <shravankumarsayz@gmail.com>
2024-05-01 09:34:04 +0530

    RMX1901: Voltage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_RMX1901.mk
+    $(LOCAL_DIR)/voltage_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    infinity_RMX1901-userdebug \
-    infinity_RMX1901-eng
+    voltage_RMX1901-userdebug \
+    voltage_RMX1901-eng
--- voltage_RMX1901.mk
+++ voltage_RMX1901.mk
@@ -7,13 +7,12 @@
 # Inherit some common DerpFest stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_NOT_USES_BLUR := true
-WITH_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
-INFINITY_MAINTAINER := Shravan
 
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,7 +22,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := infinity_RMX1901
+PRODUCT_NAME := voltage_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_SYSTEM_NAME := RMX1901
