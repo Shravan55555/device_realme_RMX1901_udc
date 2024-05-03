0224e87f5d4f3dd0bf08b779d5b5d5715beaacb4
shravansayz <shravankumarsayz@gmail.com>
2024-05-03 08:30:09 +0530

    RMX1901: Rearrange pacakge

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,31 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := GoogleCamera AdaptiveVPNPrebuilt CalendarGooglePrebuilt FilesPrebuilt PlayAutoInstallConfig SafetyHubPrebuilt talkback Updater
+LOCAL_OVERRIDES_PACKAGES := \
+		RecorderPrebuilt \
+		Photos \
+		Drive \
+		Maps \
+		YouTube \
+		MeetPrebuilt \
+		PrebuiltGmail \
+		SoundAmplifierPrebuilt \
+		GoogleCamera \
+		AdaptiveVPNPrebuilt \
+		CarrierLocation \
+		CarrierMetrics \
+		DevicePolicyPrebuilt \
+		DiagnosticsToolPrebuilt \
+		SafetyHubPrebuilt \
+		ScribePrebuilt \
+		Showcase \
+		Tycho \
+		Videos \
+		talkback \
+		Updates \
+		FM2 \
+		NfcNci \
+		AndroidAutoStubPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
