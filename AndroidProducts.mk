776ba7811cab1bf7afd13d6841c53fd9b95ffc49
shravansayz <shravankumarsayz@gmail.com>
2024-06-04 13:58:37 +0530

    RMX1901: DROID-X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/droidx_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-userdebug \
-    lineage_RMX1901-eng
+    droidx_RMX1901-userdebug \
+    droidx_RMX1901-eng
