247b51dda9d3891b54feba615addfa527aa2b32f
Shravankumar <shravankumarsayz@gmail.com>
2024-06-16 19:35:24 +0530

    RMX1901: Remove unwanted GApps Packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,31 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \
-		RecorderPrebuilt \
-		Photos \
-		Drive \
-		Maps \
-		YouTube \
-		MeetPrebuilt \
-		PrebuiltGmail \
-		SoundAmplifierPrebuilt \
-		GoogleCamera \
-		AdaptiveVPNPrebuilt \
-		CarrierLocation \
-		CarrierMetrics \
-		DevicePolicyPrebuilt \
-		DiagnosticsToolPrebuilt \
-		SafetyHubPrebuilt \
-		ScribePrebuilt \
-		Showcase \
-		Tycho \
-		Videos \
-		talkback \
-		Updates \
-		FM2 \
-		NfcNci \
-		AndroidAutoStubPrebuilt
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos Drive Maps MeetPrebuilt PrebuiltGmail SoundAmplifierPrebuilt GoogleCamera AdaptiveVPNPrebuilt CalendarGooglePrebuilt CarrierLocation CarrierMetrics DevicePolicyPrebuilt DiagnosticsToolPrebuilt FilesPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase Tycho Papers Videos PixelWallpapers2023 PixelLiveWallpaperPrebuilt talkback Updates
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
