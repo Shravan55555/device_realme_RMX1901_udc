befa3d6be48a6cf34e663e0fc8f5b19f729e732c
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-07-03 17:24:00 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone -b 14.0 --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r498229b clang
+git clone -b 14.0 --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r498229b.git
